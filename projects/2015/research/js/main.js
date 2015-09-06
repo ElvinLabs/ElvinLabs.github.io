6441ea80050e6808e4f6847d4099da32be81de98
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-09-07 01:49:46 +0530

    working without security

--- projects/2015/research/js/main.js
+++ projects/2015/research/js/main.js
@@ -3,26 +3,27 @@ function start() {
 }
 
 function postContactToGoogle() {
+		alert("called");
         var gender = $('input[name="gender"]:checked').val();
         var nationality = $('input[name="nat"]:checked').val();
         var university = $('input[name="uni"]:checked').val();
         var faculty = $('input[name="fac"]:checked').val();
         var year = $('input[name="yr"]:checked').val();
         var id = Math.floor((Math.random() * 100000) + 1); 
-
+        alert(id);
             $.ajax({
                 url: "https://docs.google.com/forms/d/1L9SncZIaGJCO2aFEWqsBEVOW-26ZVjHJlvZkhNKe8gg/formResponse",
-                data: { "entry_1406130434": gender,
-                "entry_405274992": nationality, "entry_911235950":
-                university, "entry_1366992667": faculty, "entry_87725209": year, "entry_1878655020": id },
+                data: { "entry.1406130434": gender,
+                "entry.405274992": nationality, "entry.911235950":
+                university, "entry.1366992667": faculty, "entry.87725209": year, "entry.1878655020": id },
                 type: "POST",
                 dataType: "xml",
                 statusCode: {
                     0: function () {
-                        window.location.replace("ThankYou.html");
+                        alert("0");
                     },
                     200: function () {
-                        window.location.replace("ThankYou.html");
+                        alert("200");
                     }
                 }
             });
