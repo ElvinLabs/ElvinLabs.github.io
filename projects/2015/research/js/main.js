eefff780fd6d3370f707d42889f9ba0c63d6c5f0
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-09-14 23:33:09 +0530

    scenario 2 and thanks done

--- projects/2015/research/js/main.js
+++ projects/2015/research/js/main.js
@@ -138,7 +138,7 @@ function postScenario1ToGoogle() {
         var Q = Q01+","+Q02+","+Q03+","+Q04+","+Q05+","+Q06+","+Q07+","+Q08+","+Q09+","+Q10+","+Q11+","+Q12+","+Q13+","+Q14+","+Q15+","+Q16+","+Q17+","+Q18+","+Q19+","+Q20+","+Q21+","+Q22+","+Q23+","+Q24+","+Q25+","+Q26+","+Q27+","+Q28+","+Q29+","+Q30;
         var id = (window.location).toString().split('#')[1];
          // alert(Q34);
-            $.ajax({
+            /*$.ajax({
                 url: "https://docs.google.com/forms/d/1WHU61xX_ZN9c7QDVOLu0GU2IjbuD3UPYE2NyHZznz4g/formResponse",
                 data: { 
                     "entry.1426726444": id,
@@ -149,13 +149,59 @@ function postScenario1ToGoogle() {
                 statusCode: {
                     0: function () {
                         // alert("Error 0");
-                        window.location.replace("../scenarios1/index.html#"+id);
+                        window.location.replace("../scenarios2/index.html#"+id);
                     },
                     200: function () {
                          // alert("Success 200");
-                        window.location.replace("../scenarios1/index.html#"+id);
+                        window.location.replace("../scenarios2/index.html#"+id);
                     }
                 }
-            });
-            window.location.replace("../scenarios1/index.html#"+id);
+            });*/
+            window.location.replace("../scenarios2/index.html#"+id);
+}
+
+function postScenario2ToGoogle() {
+        // alert("called");
+        var Q01 = $('input[name="q01"]:checked').val();
+        var Q02 = $('input[name="q02"]:checked').val();
+        var Q03 = $('input[name="q03"]:checked').val();
+        var Q04 = $('input[name="q04"]:checked').val();
+        var Q05 = $('input[name="q05"]:checked').val();
+        var Q06 = $('input[name="q06"]:checked').val();
+        var Q07 = $('input[name="q07"]:checked').val();
+        var Q08 = $('input[name="q08"]:checked').val();
+        var Q09 = $('input[name="q09"]:checked').val();
+        var Q10 = $('input[name="q10"]:checked').val();
+        var Q11 = $('input[name="q11"]:checked').val();
+        var Q12 = $('input[name="q12"]:checked').val();
+        var Q13 = $('input[name="q13"]:checked').val();
+        var Q14 = $('input[name="q14"]:checked').val();
+        var Q15 = $('input[name="q15"]:checked').val();
+        var Q = Q01+","+Q02+","+Q03+","+Q04+","+Q05+","+Q06+","+Q07+","+Q08+","+Q09+","+Q10+","+Q11+","+Q12+","+Q13+","+Q14+","+Q15;
+        var id = (window.location).toString().split('#')[1];
+         // alert(Q34);
+            /*$.ajax({
+                url: "https://docs.google.com/forms/d/1kUol1t5StQozVG6E8N_Vp-bQweiE17esELapKGeC2sU/formResponse",
+                data: { 
+                    "entry.782870021": id,
+                    "entry.1884208355": Q
+                },
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function () {
+                        // alert("Error 0");
+                        window.location.replace("../thanks/index.html");
+                    },
+                    200: function () {
+                        // alert("Success 200");
+                        window.location.replace("../thanks/index.html");
+                    }
+                }
+            });*/
+            window.location.replace("../thanks/index.html");
+}
+
+function end(){
+    window.open('','_self').close();
 }
