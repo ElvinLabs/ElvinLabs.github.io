9d58dc43fbb2798cd62f49d598856c0d69a6cf33
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-09-30 23:37:22 +0530

    change details

--- projects/2015/research/js/main.js
+++ projects/2015/research/js/main.js
@@ -6,18 +6,18 @@ function postContactToGoogle() {
 		// alert("called");
         var gender = $('input[name="gender"]:checked').val();
         var nationality = $('input[name="nat"]:checked').val();
-        var university = $('input[name="uni"]:checked').val();
+        var age = $('input[name="age"]:checked').val();
         var faculty = $('input[name="fac"]:checked').val();
-        var year = $('input[name="yr"]:checked').val();
+        var city = $('input[name="city"]:checked').val();
         var array = [gender,nationality,university,faculty,year];
         var id = Math.floor((Math.random() * 1000000) + 1); 
-        if(validateForm(array)){
+        if(1){
             // alert(id);
                 $.ajax({
                     url: "https://docs.google.com/forms/d/1L9SncZIaGJCO2aFEWqsBEVOW-26ZVjHJlvZkhNKe8gg/formResponse",
                     data: { "entry.1406130434": gender,
                     "entry.405274992": nationality, "entry.911235950":
-                    university, "entry.1366992667": faculty, "entry.87725209": year, "entry.1878655020": id },
+                    age, "entry.1366992667": faculty, "entry.87725209": city, "entry.1878655020": id },
                     type: "POST",
                     dataType: "xml",
                     statusCode: {
@@ -31,7 +31,7 @@ function postContactToGoogle() {
                         }
                     }
                 });
-                window.location.replace("../instructions/index.html#"+id);
+                // window.location.replace("../instructions/index.html#"+id);
         }
 }
 
