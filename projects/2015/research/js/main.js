b803415101c9c752ba90b33515d4135ad4614417
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-10-05 23:01:27 +0530

    add new form

--- projects/2015/research/js/main.js
+++ projects/2015/research/js/main.js
@@ -14,10 +14,10 @@ function postContactToGoogle() {
         if(1){
             	alert(id);
                 $.ajax({
-                    url: "https://docs.google.com/forms/d/1L9SncZIaGJCO2aFEWqsBEVOW-26ZVjHJlvZkhNKe8gg/formResponse",
-                    data: { "entry.1406130434": gender,
-                    "entry.405274992": nationality, "entry.911235950":
-                    age, "entry.1366992667": faculty, "entry.87725209": city, "entry.1878655020": id },
+                    url: "https://docs.google.com/forms/d/1IB8Ffk0TcJHu8mmFOLAtJtuF7LDqbcMCS95-RCB3n14/formResponse",
+                    data: { "entry.1871888126": gender,
+                    "entry.921556252": nationality, "entry.755141542":
+                    age, "entry.61238065": faculty, "entry.1463185923": city, "entry.530096389": id },
                     type: "POST",
                     dataType: "xml",
                     statusCode: {
